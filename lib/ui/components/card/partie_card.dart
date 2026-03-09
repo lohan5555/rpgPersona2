e14c20ccb91ac9571feaef35f61bd1913f7476b8
Lohan Hébert <lohanhebert7@gmail.com>
2026-03-09 16:44:05 +0100

    style: partie card

--- lib/ui/components/card/partie_card.dart
+++ lib/ui/components/card/partie_card.dart
@@ -18,75 +18,83 @@ class PartieCard extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: Colors.transparent,
       elevation: 0,
       margin: const EdgeInsets.only(bottom: 12),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(16),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (_) => PartieDetailPage(partie: partie, onEdit: onEdit),
-            ),
-          );
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
+      shape: ContinuousRectangleBorder(),
+      child:Column(
+        children: [
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => PartieDetailPage(partie: partie, onEdit: onEdit),
+                ),
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16, top: 16, right: 16, bottom: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    partie.emoji,
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  const SizedBox(width: 10),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Row(
                     children: [
                       Text(
-                        partie.name,
+                        partie.emoji,
                         style: Theme.of(context).textTheme.titleLarge,
                       ),
-                      const SizedBox(height: 8),
-                      Text(
-                        partie.desc ?? "",
-                        style: Theme.of(context).textTheme.bodySmall,
+                      const SizedBox(width: 20),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            partie.name,
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                          if (partie.desc != null && partie.desc!.isNotEmpty) ...[
+                            const SizedBox(height: 4),
+                            Text(
+                              partie.desc!,
+                              style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                                color: Colors.grey[600],
+                              ),
+                            ),
+                          ],
+                        ],
                       ),
                     ],
                   ),
+                  IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () => showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('Attention'),
+                        content: const Text('Êtes-vous sur de vouloir supprimer cette partie ? Cette action est définitive.'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, 'Cancel'),
+                            child: const Text('Annuler'),
+                          ),
+                          TextButton(
+                              onPressed: () => {
+                                Navigator.pop(context, 'OK'),
+                                onDelete()
+                              },
+                              child: const Text('OK')),
+                        ],
+                      ),
+                    ),
+                  )
                 ],
-              ),
-              IconButton(
-                icon: const Icon(Icons.delete),
-                onPressed: () => showDialog<String>(
-                  context: context,
-                  builder: (BuildContext context) => AlertDialog(
-                    title: const Text('Attention'),
-                    content: const Text('Êtes-vous sur de vouloir supprimer cette partie ? Cette action est définitive.'),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () => Navigator.pop(context, 'Cancel'),
-                        child: const Text('Annuler'),
-                      ),
-                      TextButton(
-                          onPressed: () => {
-                            Navigator.pop(context, 'OK'),
-                            onDelete()
-                          },
-                          child: const Text('OK')),
-                    ],
-                  ),
-                ),
               )
-            ],
-          )
-        ),
-      ),
+            ),
+          ),
+          Divider()
+        ],
+      )
     );
   }
 
